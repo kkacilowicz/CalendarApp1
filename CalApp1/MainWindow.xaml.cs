74e2f6f1614b39215277325379ec34ac3ba521e3
roxygene <62986432+roxygene@users.noreply.github.com>
2021-04-20 14:29:34 +0200

    wstępne dodawanie eventów działa
    porównywanie dni kalendarza i dni przypisanych do eventów nie działa, sorki że tak ale coś przez vs nie mogłam

--- CalApp1/MainWindow.xaml.cs
+++ CalApp1/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using CalApp1.ViewModels;
+using GalaSoft.MvvmLight.Messaging;
+using Interactive_calendar.Entities;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,7 +26,7 @@ namespace CalendarApp1
     {
 
         //private readonly Interactive_calendarDbContext _context;
-
+        Interactive_calendarDbContext context;
 
         public MainWindow()
         {
@@ -32,10 +36,49 @@ namespace CalendarApp1
         private void btnToDo_Click(object sender, RoutedEventArgs e)
         {
 
+            var newEvent = new Event()
+            {
+                Name = "nowy evencik",
+                Description = "opis",
+                DateStart = new DateTime(2021, 5, 1, 8, 30, 52),
+                DateEnd = new DateTime(2021, 5, 2, 8, 30, 52),
+                UserId = 1,
+                
+            };
+            //context.Events.Add(newEvent);
+            //context.SaveChanges();
+           
         }
 
+        private void events_btnClicked(object sender, RoutedEventArgs e)
+        {
+            DataContext = new EventsViewModel();
+            
 
-        //private void 
+        }
+
+        private void calendarMenu_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+             EventsViewModel calendarEvent = new EventsViewModel();
+             Messenger.Default.Send<DateTime>(calendarMenu.SelectedDate.Value);
+             Messenger.Default.Send<string>("test");
+             DataContext = calendarEvent;
+             
+
+
+        }
+
+        private void habitStatistics_Click(object sender, RoutedEventArgs e)
+        {
+            EventsViewModel calendarEvent = new EventsViewModel();
+            Messenger.Default.Send<DateTime>(new DateTime(2021,5,2,8,30,52));
+            DataContext = calendarEvent;
+        }
+
+        private void habitCreatorBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new RedScreenModel();
+        }
     }
 }
 
